ce2ba6100f914b52d12d6b35bc4f76dd170d8732
Sudarshan Jadhav <sudarshan@quickinfracloud.com>
2024-03-14 16:48:11 +0530

    modified code

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -5,6 +5,8 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
+  cluster_endpoint_public_access  = true
+
   cluster_addons = {
     coredns = {
       most_recent = true

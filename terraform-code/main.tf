86471874b88ad81f71973d9b8b9e5c11bebaa79e
Sudarshan Jadhav <sudarshan@quickinfracloud.com>
2024-03-15 18:39:33 +0530

    dd

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -1,11 +1,3 @@
-data "aws_eks_cluster" "cluster" {
-    name = module.eks.cluster_id
-}
-data "aws_eks_cluster_auth" "cluster" {
-    name = module.eks.cluster_id
-}
-
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"

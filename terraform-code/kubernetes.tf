587d95c4ad1982238ae7046e21ebd1829b58865a
Sudarshan Jadhav <117144308+sudarshanj01@users.noreply.github.com>
2024-03-15 18:24:04 +0530

    kubernetes.tf file add

--- terraform-code/kubernetes.tf
+++ terraform-code/kubernetes.tf
@@ -1,5 +1,14 @@
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+}
+
+# Provider configuration for Kubernetes
 provider "kubernetes" {
-    host = data.aws_eks_cluster.cluster.endpoint
-    token = data.aws_eks_cluster_auth.cluster.token
-    cluster_ca_certificate = base64encode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  host = data.aws_eks_cluster.cluster.endpoint
+  token = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64encode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
